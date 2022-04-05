b6f913fa94fbc026481c467399ba84e8bdf9c82b
eitan lev <eitanleviev111@gmail.com>
2022-04-05 23:31:55 +0300

    downgrade to unity 2020.3.30f1 to support android build + scene managing

---
+++ memoApp/Assets/scripts/Randomize.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Random = System.Random;
+
+public class Randomize : MonoBehaviour
+{
+    private int currIndex = 0;
+    private float readingTime = 1f;
+    private Random rnd = new Random();
+    private int randDig;
+    private int digNum;
+    private int Delay;
+    private bool isDelaying = false;
+    public GameObject digitTxt;
+    private AudioSource currAS;
+
+    private GameObject gameManager;
+
+    public GameObject testButton;
+
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GameManager");
+        
+        digNum = gameManager.GetComponent<GameManager>().DigitsGetter();
+        Delay = gameManager.GetComponent<GameManager>().DelayGetter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // suspension ?
+
+        if (currIndex < digNum)
+        {
+            if (! isDelaying)
+            {
+                // generate random number
+                randDig = rnd.Next(0, 9);
+                // append to list
+                gameManager.GetComponent<GameManager>().AddToRandomList(randDig);
+                // display on screen
+                digitTxt.GetComponent<Text>().text = randDig.ToString();
+                // play sound
+                GameObject.Find($"number {randDig} sound").GetComponent<AudioSource>().Play();
+                // increase index
+                currIndex++;
+                // delay
+                isDelaying = true;
+                Invoke("EndDelay", readingTime + Delay);
+            }
+        }
+        else
+        {
+            if (!isDelaying)
+            {
+                SceneManager.LoadScene("Test"); // without "test yourself" button. optional.
+                return;
+                
+                // number disappears
+                digitTxt.GetComponent<Text>().text = "";
+                // test button appears
+                testButton.SetActive(true);
+            }
+        }
+    }
+
+    private void EndDelay()
+    {
+        isDelaying = false;
+    }
+
+    public void LoadTestScene()
+    {
+        SceneManager.LoadScene("Test");
+    }
+    
+    public void LoadSetupScene()
+    {
+        SceneManager.LoadScene("setup");
+    }
+}
---
+++ memoApp/Assets/scripts/Test.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using Image = UnityEngine.UI.Image;
+
+public class Test : MonoBehaviour
+{
+    public float endTestTime;
+    
+    private int digNum;
+    private int currIndex = 0;
+    private int currNumPressed = 0;
+    private int currCorrectNumber;
+    
+    private GameObject gameManager;
+    
+    // colors
+    private GameObject regularColorObj;
+    private Color regularColor;
+    
+    // flags
+    private bool enablePressing = true;
+
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GameManager");
+
+        digNum = gameManager.GetComponent<GameManager>().DigitsGetter();
+        
+        // colors
+        regularColorObj = GameObject.Find("regular");
+        regularColor = regularColorObj.GetComponent<SpriteRenderer>().color;
+    }
+
+    public void SetCurrNumPressed(int newValue)
+    {
+        // set previous color back to the regular color
+        GameObject.Find($"{currNumPressed}").GetComponent<Image>().color = regularColor;
+        // update the current pressed number
+        currNumPressed = newValue;
+    }
+
+    public bool IsRightNumberPressed()
+    {
+        currCorrectNumber = gameManager.GetComponent<GameManager>().ListRandomAccess(currIndex);
+        currIndex++;
+        if (currIndex == digNum) // if all numbers were guessed then disable pressing
+        {
+            enablePressing = false;
+            
+            // optional : back to setup scene after endTestTime sec.
+            Invoke(nameof(LoadSetupScene), endTestTime);
+        }
+        return currCorrectNumber == currNumPressed;
+    }
+
+    public bool EnablePressingGetter()
+    {
+        return enablePressing;
+    }
+    
+    public void LoadSetupScene()
+    {
+        SceneManager.LoadScene("setup");
+    }
+}
